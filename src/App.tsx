7bed327fe759cf024b1f6c8cc2de0cf19176a314
Yash Yadav <yashyadav9325@gmail.com>
2025-02-27 15:00:08 +0530

    routes created , clerk initialised

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { PublicLayout } from "@/layouts/public-layout";
+
+import AuthenticationLayout from "@/layouts/auth-layout";
+
+import HomePage from "@/routes/home";
+import { SignIn } from "./routes/sign-in";
+import { SignUp } from "./routes/sign-up";
+
 const App = () => {
-  return <div className="text-blue-500">App</div>;
+  return (
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+        {/*authentication layout */}
+        <Route element={<AuthenticationLayout />}>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        {/* protected routes */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
