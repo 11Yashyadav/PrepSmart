11c6feeb691aec581a19b0b294a3e5829a0babd0
VarshaChandak <154354888+VarshaChandak@users.noreply.github.com>
2025-02-27 19:03:23 +0530

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,24 @@
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
+import { Container } from "./container";
+
 const Header = () => {
-  return <div>Header</div>;
+  const { userId } = useAuth();
+  return (
+    <header
+      className={cn("w-full border-b duration-150 transition-none ease-in-out")}
+    >
+      <Container>
+        <div >
+          {/*logo section  */}
+
+          {/*navigation section  */}
+
+          {/*profile section  */}
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
