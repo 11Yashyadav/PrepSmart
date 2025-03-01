8a633b02744afb1e6555bdfa9cc85336c29797bf
Yash Yadav <yashyadav9325@gmail.com>
2025-03-01 18:50:27 +0530

    firebase configuration & userdata saved successfully using handlers

--- src/handlers/auth-handler.tsx
+++ src/handlers/auth-handler.tsx
@@ -2,7 +2,7 @@ import { db } from "@/config/firebase.config";
 import { LoaderPage } from "@/routes/loader-page";
 import { User } from "@/types";
 import { useAuth, useUser } from "@clerk/clerk-react";
-import { doc, getDoc, serverTimestamp } from "firebase/firestore";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -22,10 +22,20 @@ const AutheHandler = () => {
         try {
           const userSnap = await getDoc(doc(db, "user", user.id));
           if (!userSnap.exists()) {
-            const userData: User = {};
+            const userData: User = {
+              id: user.id,
+              name: user.fullName || user.firstName || "Anonymous",
+              email: user.primaryEmailAddress?.emailAddress || "N/A",
+              imageURL: user.imageUrl,
+              createdAt: serverTimestamp(),
+              updateAt: serverTimestamp(),
+            };
+            await setDoc(doc(db, "user", user.id), userData);
           }
         } catch (error) {
           console.log("Error on Storing the user data : ", error);
+        } finally {
+          setLoading(false);
         }
       }
     };
