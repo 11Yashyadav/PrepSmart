5c3bea06bf058507d3e12c2a9b7df4c70ed02b52
Yash Yadav <yashyadav9325@gmail.com>
2025-03-01 18:30:18 +0530

    Handler creation started

---
+++ src/handlers/auth-handler.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/config/firebase.config";
+import { LoaderPage } from "@/routes/loader-page";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const AutheHandler = () => {
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+
+  const pathname = useLocation().pathname;
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const storeUserData = async () => {
+      if (isSignedIn && user) {
+        setLoading(true);
+        try {
+          const userSnap = await getDoc(doc(db, "user", user.id));
+          if (!userSnap.exists()) {
+            const userData: User;
+          }
+        } catch (error) {
+          console.log("Error on Storing the user data : ", error);
+        }
+      }
+    };
+    storeUserData();
+  }, [isSignedIn, user, pathname, navigate]);
+
+  if (loading) {
+    return <LoaderPage />;
+  }
+  return null;
+};
+
+export default AutheHandler;
