3aed5cc5e8e18b333f69acc6732a89737c178b32
Yash Yadav <yashyadav9325@gmail.com>
2025-03-02 20:30:29 +0530

    working on home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,29 @@
+import { Container } from "@/components/container";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="felx-col w-full pb-24">
+      <Container>
+        <div className="my-8">
+          <h2 className="text-3xl text-center md:text-left md:text-6xl">
+            <span className="text-outline font-extrabold md:text-8xl">
+              AI SuperPower
+            </span>
+            <span className="text-gray-500 font-extrabold">
+              - A better way to
+            </span>
+            <br />
+            improve your interview chances and skills
+          </h2>
+          <p className="mt-4 text-muted-foreground text-sm">
+            Boost your interview skills and increase your success rate with
+            AI-driven insights. Discover a smarter way to prepare,practice and
+            stand out.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HomePage;
