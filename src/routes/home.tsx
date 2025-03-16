137d46425b01c50e09fc8735ef2ae5534c7a7046
Yash Yadav <yashyadav9325@gmail.com>
2025-03-16 17:34:07 +0530

    image section is added in Home Page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,6 @@
 import { Container } from "@/components/container";
+import { Button } from "@/components/ui/button";
+import { Sparkles } from "lucide-react";
 
 const HomePage = () => {
   return (
@@ -20,6 +22,30 @@ const HomePage = () => {
             AI-driven insights. Discover a smarter way to prepare,practice and
             stand out.
           </p>
+        </div>
+        {/* image section */}
+        <div className="w-full mt-4 rounded-xl bg-gray-100 h-[420px] drop-shadow-md overflow-hidden relative">
+          <img
+            src="/assets/img/hero.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute top-4 left-4 px-4 py-2 rounded-md bg-white/40 backdrop-blur-md">
+            Inteviews Copilot&copy;
+          </div>
+
+          <div className="hidden md:block absolute w-80 bottom-4 right-4 px-4 py-2 rounded-md bg-white/60 backdrop-blur-md">
+            <h2 className="text-neutral-800 font-semibold">Developer</h2>
+            <p className="text-sm text-neutral-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              distinctio natus, quos voluptatibus magni sapiente.
+            </p>
+
+            <Button className="mt-3">
+              Generate <Sparkles />
+            </Button>
+          </div>
         </div>
       </Container>
     </div>
