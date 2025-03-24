b4db5dc2803775d410ee61ff09c5e26689afda10
Yash Yadav <yashyadav9325@gmail.com>
2025-03-24 18:46:21 +0530

    speech to text is implemented using react-hook

--- src/components/record-answer.tsx
+++ src/components/record-answer.tsx
@@ -1,3 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useAuth } from "@clerk/clerk-react";
+
+import { useEffect, useState } from "react";
+import useSpeechToText, { ResultType } from "react-hook-speech-to-text";
+import { useParams } from "react-router-dom";
+import WebCam from "react-webcam";
+import { TooltipButton } from "./tooltip-button";
+import { toast } from "sonner";
+import { chatSession } from "@/scripts";
+import { SaveModal } from "./save-modal";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+import { db } from "@/config/firebase.config";
 
 interface RecordAnswerProps {
   question: { question: string; answer: string };
@@ -5,12 +25,141 @@ interface RecordAnswerProps {
   setIsWebCam: (value: boolean) => void;
 }
 
+interface AIResponse {
+  ratings: number;
+  feedback: string;
+}
+// * ****
 export const RecordAnswer = ({
   question,
   isWebCam,
   setIsWebCam,
 }: RecordAnswerProps) => {
-   = useSpeechToText({
+  const {
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
     continuous: true,
     useLegacyResults: false,
   });
+
+  const [userAnswer, setUserAnswer] = useState("");
+  const [isAiGenerating, setIsAiGenerating] = useState(false);
+  const [aiResult, setAiResult] = useState<AIResponse | null>(null);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { userId } = useAuth();
+  const { interviewId } = useParams();
+
+  const recordUserAnswer = async () => {
+    if (isRecording) {
+      stopSpeechToText();
+
+      if (userAnswer?.length < 30) {
+        toast.error("Error", {
+          description: "Your answer should be more than 30 characters",
+        });
+
+        return;
+      }
+
+      //   ai result
+      const aiResult = await generateResult(
+        question.question,
+        question.answer,
+        userAnswer
+      );
+
+      setAiResult(aiResult);
+    } else {
+      startSpeechToText();
+    }
+  };
+
+  // **************************
+
+  useEffect(() => {
+    const combineTranscripts = results
+      .filter((result): result is ResultType => typeof result !== "string")
+      .map((result) => result.transcript)
+      .join(" ");
+
+    setUserAnswer(combineTranscripts);
+  }, [results]);
+
+  // *************************************
+  return (
+    <div className="w-full flex flex-col items-center gap-8 mt-4">
+      {/* save modal */}
+      <SaveModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={saveUserAnswer}
+        loading={loading}
+      />
+
+      <div className="w-full h-[400px] md:w-96 flex flex-col items-center justify-center border p-4 bg-gray-50 rounded-md">
+        {isWebCam ? (
+          <WebCam
+            onUserMedia={() => setIsWebCam(true)}
+            onUserMediaError={() => setIsWebCam(false)}
+            className="w-full h-full object-cover rounded-md"
+          />
+        ) : (
+          <WebcamIcon className="min-w-24 min-h-24 text-muted-foreground" />
+        )}
+      </div>
+
+      <div className="flex itece justify-center gap-3">
+        <TooltipButton
+          content={isWebCam ? "Turn Off" : "Turn On"}
+          icon={
+            isWebCam ? (
+              <VideoOff className="min-w-5 min-h-5" />
+            ) : (
+              <Video className="min-w-5 min-h-5" />
+            )
+          }
+          onClick={() => setIsWebCam(!isWebCam)}
+        />
+
+        <TooltipButton
+          content={isRecording ? "Stop Recording" : "Start Recording"}
+          icon={
+            isRecording ? (
+              <CircleStop className="min-w-5 min-h-5" />
+            ) : (
+              <Mic className="min-w-5 min-h-5" />
+            )
+          }
+          onClick={recordUserAnswer}
+        />
+
+        <TooltipButton
+          content="Record Again"
+          icon={<RefreshCw className="min-w-5 min-h-5" />}
+          onClick={recordNewAnswer}
+        />
+      </div>
+
+      <div className="w-full mt-4 p-4 border rounded-md bg-gray-50">
+        <h2 className="text-lg font-semibold">Your Answer:</h2>
+
+        <p className="text-sm mt-2 text-gray-700 whitespace-normal">
+          {userAnswer || "Start recording to see your ansewer here"}
+        </p>
+
+        {interimResult && (
+          <p className="text-sm text-gray-500 mt-2">
+            <strong>Current Speech:</strong>
+            {interimResult}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
